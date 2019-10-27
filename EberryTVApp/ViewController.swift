5bb4cd19d584a864660ea291dc1f60e9235818d0
Mohamed Alwakil <mdalwakil@outlook.com>
2019-10-27 11:38:28 +0100

    Initial Commit

---
+++ EberryTVApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EberryTVApp
+//
+//  Created by Mohamed Diaa on 2019-10-27.
+//  Copyright © 2019 Mohamed Diaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EberryTVAppTests/EberryTVAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  EberryTVAppTests.swift
+//  EberryTVAppTests
+//
+//  Created by Mohamed Diaa on 2019-10-27.
+//  Copyright © 2019 Mohamed Diaa. All rights reserved.
+//
+
+import XCTest
+@testable import EberryTVApp
+
+class EberryTVAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
